73cf8b93e490288e75ce711df72dedd3068289f0
Alvin Murphy <amurp003@odu.edu>
2022-02-10 16:56:52 -0500

    Added tracer with Jaeger

--- src/dss-prototype/app.py
+++ src/dss-prototype/app.py
@@ -1,45 +1,81 @@
-from flask import Flask
-from flask import render_template
+import flask
 import requests # https://realpython.com/api-integration-in-python/
+from flask import render_template
 
-app = Flask(__name__)
+from opentelemetry import trace
+# from opentelemetry.instrumentation.flask import FlaskInstrumentor
+# from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+
+# Set up OpenTelemetry in Flask (see #flask_example.py)
+# from https://opentelemetry.io/docs/instrumentation/python/getting-started/
+
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "dss-prototype"})
+    )
+)
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name="localhost",
+    agent_port=6831,
+)
+
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(jaeger_exporter)
+)
+
+app = flask.Flask(__name__)
+# FlaskInstrumentor().instrument_app(app)
+# RequestsInstrumentor().instrument()
+
+tracer = trace.get_tracer(__name__)
 
 @app.route('/')
 def ui():
-    return render_template('leaflet-map-ui.html')
+    with tracer.start_as_current_span("rendermap") as span:
+        return render_template('leaflet-map-ui.html')
 
 @app.route('/RIC')
 def get_ric_flights():
-    # Latitude = N/S, Longitude = E/W
-    # 1 deg = 60 NM Lat, varies with Lon
-    # N and E are positive
+    with tracer.start_as_current_span("RIC Flight Data"):
+        # Latitude = N/S, Longitude = E/W
+        # 1 deg = 60 NM Lat, varies with Lon
+        # N and E are positive
 
-    #RIC 37.5407 N, 77.4360 W
-    # Richmond box
-    # lamin=37.5407 - 1 =36.5407
-    # lomin=-77.4360 - 1=-78.4360 W
-    # lamax=37.5407 + 1 = 38.5407 N
-    # lomax=-77.4360 + 1 = -76.4360 W
+        #RIC 37.5407 N, 77.4360 W
+        # Richmond box
+        # lamin=37.5407 - 1 =36.5407
+        # lomin=-77.4360 - 1=-78.4360 W
+        # lamax=37.5407 + 1 = 38.5407 N
+        # lomax=-77.4360 + 1 = -76.4360 W
 
-    url_RIC = "https://opensky-network.org/api/states/all?lamin=36.5407&lomin=-78.4360&lamax=38.5407&lomax=-76.4360"
+        url_RIC = "https://opensky-network.org/api/states/all?lamin=36.5407&lomin=-78.4360&lamax=38.5407&lomax=-76.4360"
    
-    api_url = url_RIC
-    response = requests.get(api_url)
-    return (response.json())
+        api_url = url_RIC
+        response = requests.get(api_url)
+        return (response.json())
 
 @app.route('/IAD')
 def get_iad_flights():
-    # Latitude = N/S, Longitude = E/W
-    # 1 deg = 60 NM Lat, varies with Lon
-    # N and E are positive
+    with tracer.start_as_current_span("IAD Flight Data"):
+        # Latitude = N/S, Longitude = E/W
+        # 1 deg = 60 NM Lat, varies with Lon
+        # N and E are positive
 
-    # IAD 38.9531 N, 77.4565 W 
+        # IAD 38.9531 N, 77.4565 W 
 
-    url_IAD = "https://opensky-network.org/api/states/all?lamin=37.9537&lomin=-78.4565&lamax=39.9537&lomax=-76.4565"
+        url_IAD = "https://opensky-network.org/api/states/all?lamin=37.9537&lomin=-78.4565&lamax=39.9537&lomax=-76.4565"
 
-    api_url = url_IAD
-    response = requests.get(api_url)
-    return (response.json())
+        api_url = url_IAD
+        response = requests.get(api_url)
+        return (response.json())
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+app.run(port=5000)
---
+++ src/software-experiments/flask_example.py
@@ -0,0 +1,33 @@
+# flask_example.py
+import flask
+import requests
+
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(ConsoleSpanExporter())
+)
+
+app = flask.Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+tracer = trace.get_tracer(__name__)
+
+
+@app.route("/")
+def hello():
+    with tracer.start_as_current_span("example-request"):
+        requests.get("http://www.example.com")
+    return "hello"
+
+
+app.run(port=5000)
