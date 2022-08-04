eb44a7488ea41b4a5120c142cd7716222dd60cfc
Alvin Murphy <amurp003@odu.edu>
2022-08-03 22:06:25 -0400

    Change io counters to see delta

--- src/test-app/test-app.py
+++ src/test-app/test-app.py
@@ -159,7 +159,7 @@ def run_tests(num_tests: int = 5, num_requests: int = 5,
             
             span.set_attribute("cpu.load.avg", loadavg)
             span.set_attribute("num.cpu", numcpu)
-            span.set_attribute("net.io.count", net_io_count)
+            span.set_attribute("net.io.count.removeme", net_io_count)
             span.add_event( "start test", {
                 "test.number": (test + 1),
                 "num.tests": num_tests,
